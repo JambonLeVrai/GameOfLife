eb109ed6118a43e438e7e6e132829a926661a3f5
armin <thomas.lejeune@ens-paris-saclay.fr>
2026-02-11 11:53:59 +0100

    remove color arg in Cell

--- cell.py
+++ cell.py
@@ -8,20 +8,12 @@ class Cell:
     y: int
     status_actual: int = 0
     status_next: int = 0
-    color: int = 255
-
-    """def __post_init__(self):
-        if (self.status_actual > max_status) or (self.status_actual < min_status):
-            raise ValueError(
-            f"status_actual={self.status_actual} outside the interval [{min_status}, {max_status}]"
-        )"""
 
     def update(self):
         self.status_actual = self.status_next
 
     def __repr__(self):
-        return str(self.status_next)#f"Cell(x:{self.x}, y:{self.y}, actual status: {self.status_actual}, next turn status: {
-    # self.status_next})"
+        return str(self.status_next)
 
 
 if __name__ == '__main__':
@@ -30,6 +22,5 @@ if __name__ == '__main__':
         y=2,
         status_actual=3,
         status_next=0,
-        color=(0, 0, 0)
     )
     print(cell)
